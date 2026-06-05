a0f09857ce1abeb2503a3c89b5ba0d32a6ec59c4
Samir Prusty <samir.rp19@gmail.com>
2026-06-05 10:07:36 +0530

    new code

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express ()
+const PORT = process.env.port ?? 7000
+
+app.get('/', (req, res) => {
+    return res.json({ msg : 'Hello from the server'})
+})
+
+app.listen(PORT, () => {
+    console.log(`server is up and running on PORT ${PORT}`)
+})
